b60b7ad6b972bd4cc97de77ba19e1610fb027165
Neo2003-TBC <Neo2003-TBC@6c1fcba4-eb2c-0410-9096-d29676c3c6e1>
2008-05-25 20:26:44 +0000

    UDB release 0.10.2: "Fishermans Friend"
    Biggest changes in 351:
    - Completely re-done fishing loot template.
    - Fishing base skill level table added
    - Lots of Black Temple updates
    - Loot added for creatures that are mineable and gatherable
    - Rewards added for quests with quest flags|512 (Quests like Teron Gorefiend I am!)
    - Many quest fixes in general
    - 175 fishing holes added (like school of fish) with proper loot
    - Instance loot tables updated: Zul'Farrak, Sunken Temple, Black Rock Depths, Dire Maul, Black Rock Spire,
    - World drop loot tables for levels 1-34 created (SIGNIFICANT decrease in creature_loot_template size again. All world drop tables are now created. Loot table size before world tables ~950000 rows -- after 284594 rows. ~7mB instead of ~22mB)
    - Some 2.4.2 related item changes
    - L70ETC spawns / event included in the DB (still need the ACID script from the ACID team)
    - Plenty of fixes from forums, thanks to all contributors!
    - *_template scripts added for SD2 up to 414 and ACID 0.0.1 r10
    
    For the ones who don't want to install a full DB, you can update from rev 350 by using 351 files in tags/0.10.1/Updates
    
    This release is compatible with core 5949+

---
+++ Updates/323_corepatch_4613_to_4627.sql
@@ -0,0 +1,12 @@
+ALTER TABLE `creature_template`
+ ADD COLUMN `modelid_A2` INTEGER UNSIGNED NOT NULL DEFAULT 0 AFTER `modelid_A`,
+ ADD COLUMN `modelid_H2` INTEGER UNSIGNED NOT NULL DEFAULT 0 AFTER `modelid_H`;
+
+DROP TABLE IF EXISTS `game_event_model_equip`;
+CREATE TABLE `game_event_model_equip` (
+  `guid` int(11) unsigned NOT NULL DEFAULT '0',
+  `modelid` int(11) unsigned NOT NULL DEFAULT '0',
+  `equipment_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `event` mediumint(9) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
